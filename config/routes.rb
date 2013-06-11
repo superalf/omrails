2398ee0b239689e9443c6d39cd6b8270307f7d10
fedor peter <fedor.peter@gmail.com>
2013-06-11 16:43:26 +0200

    Add Bootsrtap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Omrails::Application.routes.draw do
+  
+  get "pages/home"
+
+  root :to => 'pages#home'
+
+  get 'about' => 'pages#about'
+
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,7 @@ Omrails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
